f244d2992260277fc9dd3d6450c49572fbb89aef
phyuzinzaw <phyuzinzaw644@gmail.com>
2018-08-10 22:38:20 +0630

    upload heroku

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('Home.html')
+
+@app.route('/author')
+def author():
+    return render_template('Author.html')
+
+@app.route('Layout')
+def Layout():
+    return render_template('Layout.html')
+
+@app.route('Search')
+def Search():
+    return render_template('Search.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
